4c50d9d49b5011a4a528670e67cda0c52a0195c7
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-10-18 19:15:58 +0300

    Made markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Profile } from './profile/Profile'
+import  user from '../assets/user.json'
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../assets/data.json'
+
+import { FriendList } from './friendsList/FriendsList';
+import friends from '../assets/friends.json'
+
+import { TransactionHistory } from './transactions/Transactions'; 
+import transactions from '../assets/transactions.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/friendsList/FriendsList.jsx
@@ -0,0 +1,23 @@
+import { FriendsListItem } from "../friendsList/FriendsListItem";
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendsListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
